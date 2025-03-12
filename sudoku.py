582c385f0281ef7195bc9a86673cd31f6768fa60
Afik Aharon <afik3248@gmail.com>
2025-03-12 17:33:43 +0200

    prettier look

--- sudoku.py
+++ sudoku.py
@@ -10,73 +10,64 @@ board = [
     [0,4,9,2,0,6,0,0,7]
 ]
 
-
 def solve(bo):
     find = find_empty(bo)
     if not find:
-        return True
-    else:
-        row, col = find
+        return True  # No empty spots, puzzle solved
+    row, col = find
 
-    for i in range(1,10):
-        if valid(bo, i, (row, col)):
-            bo[row][col] = i
+    for num in range(1, 10):
+        if valid(bo, num, (row, col)):
+            bo[row][col] = num  # Place number
 
-            if solve(bo):
+            if solve(bo):  # Recursively solve
                 return True
 
-            bo[row][col] = 0
+            bo[row][col] = 0  # Undo move (backtrack)
 
     return False
 
-
 def valid(bo, num, pos):
+    row, col = pos
+
     # Check row
-    for i in range(len(bo[0])):
-        if bo[pos[0]][i] == num and pos[1] != i:
-            return False
+    if num in bo[row]:
+        return False
 
     # Check column
-    for i in range(len(bo)):
-        if bo[i][pos[1]] == num and pos[0] != i:
-            return False
+    if num in [bo[i][col] for i in range(9)]:
+        return False
 
-    # Check box
-    box_x = pos[1] // 3
-    box_y = pos[0] // 3
-
-    for i in range(box_y*3, box_y*3 + 3):
-        for j in range(box_x * 3, box_x*3 + 3):
-            if bo[i][j] == num and (i,j) != pos:
+    # Check 3x3 box
+    box_x = col // 3 * 3
+    box_y = row // 3 * 3
+    for i in range(3):
+        for j in range(3):
+            if bo[box_y + i][box_x + j] == num:
                 return False
 
     return True
 
-
 def print_board(bo):
-    for i in range(len(bo)):
+    for i in range(9):
         if i % 3 == 0 and i != 0:
-            print("- - - - - - - - - - - - - ")
+            print("-" * 21)
 
-        for j in range(len(bo[0])):
+        for j in range(9):
             if j % 3 == 0 and j != 0:
                 print(" | ", end="")
 
-            if j == 8:
-                print(bo[i][j])
-            else:
-                print(str(bo[i][j]) + " ", end="")
-
+            print(bo[i][j] if bo[i][j] != 0 else ".", end=" ")
+        print()
 
 def find_empty(bo):
-    for i in range(len(bo)):
-        for j in range(len(bo[0])):
+    for i in range(9):
+        for j in range(9):
             if bo[i][j] == 0:
-                return (i, j)  # row, col
-
+                return (i, j)  # Return first empty cell found
     return None
 
 print_board(board)
 solve(board)
-print("___________________\n")
+print("\nSolved Sudoku:\n")
 print_board(board)
